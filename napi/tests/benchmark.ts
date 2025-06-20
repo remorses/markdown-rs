871037f974acc1086a6a12b4f2158d781021c90a
remorses <beats.by.morse@gmail.com>
2025-06-20 11:07:48 +0200

    better benchmark, use the zod markdown long doc

--- napi/tests/benchmark.ts
+++ napi/tests/benchmark.ts
@@ -1,33 +1,63 @@
 import { run, bench, boxplot, summary } from "mitata";
-import { parse } from "../";
+import { parse, toHtml } from "../";
 import { remark } from "remark";
 import remarkMdx from "remark-mdx";
 import remarkGfm from "remark-gfm";
+import remarkHtml from "remark-html";
 
-
-const remarkProcessor = remark().use(remarkMdx).use(remarkGfm);
+const remarkProcessor = remark().use(remarkMdx).use(remarkGfm).use(remarkHtml);
 
 async function main() {
-  const longMdxContent = await fetch(
+  let longMdxContent = await fetch(
     "https://raw.githubusercontent.com/colinhacks/zod/0a49fa39348b7c72b19ddedc3b0f879bd395304b/packages/docs/content/packages/v3.mdx",
   ).then((x) => x.text());
 
-  boxplot(async () => {
-    summary(() => {
-      bench("markdown-rs parse", () => {
-        const res = parse(longMdxContent, {
+  summary(() => {
+    bench("markdown-rs parse", () => {
+      const res = parse(longMdxContent, {
+        mdx: true,
+        mdxExpressionParse: true,
+        mdxEsmParse: true,
+        gfm: true,
+      });
+    });
+
+    bench("remark parse", () => {
+      remarkProcessor.processSync(longMdxContent);
+    });
+  });
+  summary(() => {
+    bench("markdown-rs parse without parsing js", () => {
+      const res = parse(longMdxContent, {
+        mdx: true,
+        mdxExpressionParse: false,
+        mdxEsmParse: false,
+        gfm: true,
+      });
+    });
+
+    bench("remark parse", () => {
+      remarkProcessor.processSync(longMdxContent);
+    });
+  });
+  summary(() => {
+    bench("markdown-rs html", () => {
+      const res = toHtml(longMdxContent, {
+        parse: {
           mdx: true,
           mdxExpressionParse: true,
           mdxEsmParse: true,
           gfm: true,
-        });
-      });
-
-      bench("remark + remark-mdx parse", () => {
-        remarkProcessor.processSync(longMdxContent);
+        },
       });
     });
+
+    bench("remark html", () => {
+      const file = remarkProcessor.processSync(longMdxContent);
+      const res = String(file);
+    });
   });
+
   await run();
 }
 
